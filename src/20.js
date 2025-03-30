8c1ffe8645f801943ec53a11b8ad07138537f4c2
rutskiurenda360 <rutskiurenda360@gmail.com>
2025-03-30 05:25:43 +0000

    Add 20.js

---
+++ src/20.js
@@ -0,0 +1,15 @@
+function sum(a, b) {
+  return a + b;
+}
+
+function difference(a, b) {
+  return a - b;
+}
+
+function product(x, y) {
+  return x * y;
+}
+
+function quotient(numerator, denominator) {
+  return numerator / denominator;
+}
